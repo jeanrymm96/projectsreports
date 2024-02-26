e4c1a1353ffce4e76b3cf3fa7d111eb9d896e6db
jeanrymm96 <study.python96@gmail.com>
2024-02-26 16:21:32 +0700

    Project Reports Modul

---
+++ resources/views/home.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Reports Module OJK</title>
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+</head>
+<body>
+    <header>
+        <ul class="menu-header">
+            <li class="menu-item">
+                <a href="" class="item-link">Home</a>
+            </li>
+            <li class="menu-item">
+                <a href="" class="item-link">Modules</a>
+                <ul class="dropdown-menu">
+                    <li class="item-dropdown">
+                        <a href="" class="item-link-dropdown">Module LAPBUL</a>
+                    </li>
+                    <li class="item-dropdown">
+                        <a href="" class="item-link-dropdown">Module RBB</a>
+                    </li>
+                    <li class="item-dropdown">
+                        <a href="" class="item-link-dropdown">Module PR</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="menu-item">
+                <a href="" class="item-link">Help</a>
+            </li>
+            <li class="menu-item">
+                <a href="" class="item-link">About Me</a>
+            </li>
+            <li class="menu-item">
+                <a href="" class="item-link">Portofolios</a>
+            </li>
+        </ul>
+    </header>
+</body>
+</html>
