d0bb31f930dbf553fb7ae3f9d160471957e792fc
jeanrymm96 <study.python96@gmail.com>
2024-03-04 16:22:04 +0700

    Update CSSS

--- resources/views/Profiles/index.blade.php
+++ resources/views/Profiles/index.blade.php
@@ -8,29 +8,29 @@
     <link rel="stylesheet" href="{{ asset('css/profile/Styles.css')}}">
 </head>
 <body>
-    <header>
-        <ul class="menu">
-            <li class="menu-item">
-                <a href="#profile" class="item-link">Profile</a>
-            </li>
-            <li class="menu-item">
-                <a href="#contact" class="item-link">Contact</a>
-            </li>
-            <li class="menu-item">
-                <a href="#experience" class="item-link">Experiences</a>
-            </li>
-            <li class="menu-item">
-                <a href="#skill" class="item-link">Skills</a>
-            </li>
-            <li class="menu-item">
-                <a href="#language" class="item-link">Language</a>
-            </li>
-            <li class="menu-item">
-                <a href="#references" class="item-link">Reference</a>
-            </li>
-        </ul>
-    </header>
     <div class="container">
+        <header>
+            <ul class="menu">
+                <li class="menu-item">
+                    <a href="#profile" class="item-link">Profile</a>
+                </li>
+                <li class="menu-item">
+                    <a href="#contact" class="item-link">Contact</a>
+                </li>
+                <li class="menu-item">
+                    <a href="#experience" class="item-link">Experiences</a>
+                </li>
+                <li class="menu-item">
+                    <a href="#skill" class="item-link">Skills</a>
+                </li>
+                <li class="menu-item">
+                    <a href="#language" class="item-link">Language</a>
+                </li>
+                <li class="menu-item">
+                    <a href="#references" class="item-link">Reference</a>
+                </li>
+            </ul>
+        </header>
         <div class="card">
             <div class="card-body" id="profile">
                 <div class="title-card">
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,66 +8,68 @@
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
 </head>
 <body>
-    <header>
-        <ul class="menu-header">
-            <li class="menu-item">
-                <a href="" class="item-link">Home</a>
-            </li>
-            <li class="menu-item">
-                <span>Modules</span>
-                <ul class="dropdown-menu">
-                    <li class="item-dropdown">
-                        <a href="#" class="item-link-dropdown">Module LAPBUL</a>
-                    </li>
-                    <li class="item-dropdown">
-                        <a href="#" class="item-link-dropdown">Module RBB</a>
-                    </li>
-                    <li class="item-dropdown">
-                        <a href="#" class="item-link-dropdown">Module PR</a>
-                    </li>
-                    <li class="item-dropdown">
-                        <a href="#" class="item-link-dropdown">Module GCG</a>
-                    </li>
-                    <li class="item-dropdown">
-                        <a href="#" class="item-link-dropdown">Module OBOX</a>
-                    </li>
-                </ul>
-            </li>
-            <li class="menu-item">
-                <a href="{{ route('blog') }}" class="item-link">Blog</a>
-            </li>
-            <li class="menu-item">
-                <a href="{{ route('aboutMe')}}" class="item-link">About Me</a>
-            </li>
-            <li class="menu-item">
-                <span>Portofolios</span>
-                <ul class="dropdown-menu">
-                    <li class="item-dropdown">
-                        <a href="https://github.com/jeanrymm96/projectsreports" class="item-link-dropdown">Module Reports OJK</a>
-                    </li>
-                    <li class="item-dropdown">
-                        <a href="https://github.com/jeanrymm96/appsAuto" class="item-link-dropdown">Auto-Debet</a>
-                    </li>
-                    <li class="item-dropdown">
-                        <a href="https://github.com/jeanrymm96/obox" class="item-link-dropdown">OBOX</a>
-                    </li>
-                </ul>
-            </li>
-            <li class="menu-item">
-                <span>Social Media</span>
-                <ul class="dropdown-menu">
-                    <li class="item-dropdown">
-                        <a href="https://www.instagram.com/" class="item-link-dropdown">Instagram</a>
-                    </li>
-                    <li class="item-dropdown">
-                        <a href="https://www.linkedin.com/" class="item-link-dropdown">LinkedIn</a>
-                    </li>
-                    <li class="item-dropdown">
-                        <a href="https://web.telegram.org/k/" class="item-link-dropdown">Telegram</a>
-                    </li>
-                </ul>
-            </li>
-        </ul>
-    </header>
+    <div class="container">
+        <header>
+            <ul class="menu-header">
+                <li class="menu-item">
+                    <a href="" class="item-link">Home</a>
+                </li>
+                <li class="menu-item">
+                    <span>Modules</span>
+                    <ul class="dropdown-menu">
+                        <li class="item-dropdown">
+                            <a href="#" class="item-link-dropdown">Module LAPBUL</a>
+                        </li>
+                        <li class="item-dropdown">
+                            <a href="#" class="item-link-dropdown">Module RBB</a>
+                        </li>
+                        <li class="item-dropdown">
+                            <a href="#" class="item-link-dropdown">Module PR</a>
+                        </li>
+                        <li class="item-dropdown">
+                            <a href="#" class="item-link-dropdown">Module GCG</a>
+                        </li>
+                        <li class="item-dropdown">
+                            <a href="#" class="item-link-dropdown">Module OBOX</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="menu-item">
+                    <a href="{{ route('blog') }}" class="item-link">Blog</a>
+                </li>
+                <li class="menu-item">
+                    <a href="{{ route('aboutMe')}}" class="item-link">About Me</a>
+                </li>
+                <li class="menu-item">
+                    <span>Portofolios</span>
+                    <ul class="dropdown-menu">
+                        <li class="item-dropdown">
+                            <a href="https://github.com/jeanrymm96/projectsreports" class="item-link-dropdown">Module Reports OJK</a>
+                        </li>
+                        <li class="item-dropdown">
+                            <a href="https://github.com/jeanrymm96/appsAuto" class="item-link-dropdown">Auto-Debet</a>
+                        </li>
+                        <li class="item-dropdown">
+                            <a href="https://github.com/jeanrymm96/obox" class="item-link-dropdown">OBOX</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="menu-item">
+                    <span>Social Media</span>
+                    <ul class="dropdown-menu">
+                        <li class="item-dropdown">
+                            <a href="https://www.instagram.com/" class="item-link-dropdown">Instagram</a>
+                        </li>
+                        <li class="item-dropdown">
+                            <a href="https://www.linkedin.com/" class="item-link-dropdown">LinkedIn</a>
+                        </li>
+                        <li class="item-dropdown">
+                            <a href="https://web.telegram.org/k/" class="item-link-dropdown">Telegram</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </header>
+    </div>
 </body>
 </html>
